09a2ca60fc1c1f305762cc6886043c4d8b206ced
folospior <folosp2@gmail.com>
2024-07-19 22:38:00 +0200

    Create slash command when the bot is in a Ready state.

--- CIDBot/OnReady.cs
+++ CIDBot/OnReady.cs
@@ -1,12 +1,27 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Discord;
+using Discord.WebSocket;
+using Microsoft.Extensions.DependencyInjection;
 
 namespace CIDBot
 {
-    internal class OnReady
+    internal class OnReady(ServiceProvider serviceProvider)
     {
+        readonly DiscordSocketClient Client = serviceProvider.GetRequiredService<DiscordSocketClient>();
+
+        public async Task ClientReadyAsync()
+        {
+            var bgcheckCommand = new SlashCommandBuilder()
+                .WithName("bgcheck")
+                .WithDescription("Background check a Roblox user")
+                .AddOption(
+                    name: "username",
+                    description: "The username of the user you wish to background check",
+                    isRequired: true,
+                    type: ApplicationCommandOptionType.String
+                )
+                .Build();
+
+            await Client.CreateGlobalApplicationCommandAsync(bgcheckCommand);
+        }
     }
 }
