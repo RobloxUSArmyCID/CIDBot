163fabd98c54221dfab83fa6060ffa1078868807
folospior <folosp2@gmail.com>
2024-07-19 13:19:10 +0200

    Add DI Required NuGET Package + basic Main implementation

--- CIDBot/Program.cs
+++ CIDBot/Program.cs
@@ -1,9 +1,38 @@
-﻿namespace CIDBot;
+﻿using Discord;
+using Discord.WebSocket;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace CIDBot;
 
 class Program
 {
-    public static async Task Main(string[] args)
+    static readonly ServiceProvider _serviceProvider = CreateServices();
+
+    static ServiceProvider CreateServices()
     {
-        
+        var clientConfig = new DiscordSocketConfig()
+        {
+            AlwaysDownloadUsers = true,
+            GatewayIntents = GatewayIntents.AllUnprivileged | GatewayIntents.MessageContent
+        };
+
+        var collection = new ServiceCollection()
+            .AddSingleton(clientConfig)
+            .AddSingleton<DiscordSocketClient>();
+
+        return collection.BuildServiceProvider();
+    }
+
+    public static async Task Main()
+    {
+        var client = _serviceProvider.GetRequiredService<DiscordSocketClient>();
+
+        string token = Environment.GetEnvironmentVariable("CIDBot_TOKEN")
+            ?? throw new NotImplementedException("Please define the CIDBot_TOKEN environment variable");
+
+        await client.LoginAsync(TokenType.Bot, token);
+        await client.StartAsync();
+
+        await Task.Delay(-1);
     }
 }
