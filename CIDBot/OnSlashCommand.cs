4fa45c39f30eb0c26c252b5bd59a0b8578874324
folospior <folosp2@gmail.com>
2024-07-19 22:45:15 +0200

    Base Slash Command handler + command that does nothing
    Part 1 - OnSlashCommand.cs

--- CIDBot/OnSlashCommand.cs
+++ CIDBot/OnSlashCommand.cs
@@ -1,12 +1,20 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Discord.WebSocket;
+using Microsoft.Extensions.DependencyInjection;
 
 namespace CIDBot
 {
-    internal class OnSlashCommand
+    internal class OnSlashCommand(ServiceProvider serviceProvider)
     {
+        readonly DiscordSocketClient Client = serviceProvider.GetRequiredService<DiscordSocketClient>();
+
+        public async Task HandleSlashCommand(SocketSlashCommand cmd)
+        {
+            if (cmd.CommandName == "bgcheck") await OnBgcheckCommand(cmd);
+        }
+
+        async Task OnBgcheckCommand(SocketSlashCommand cmd)
+        {
+            await cmd.DeferAsync();
+        }
     }
 }
