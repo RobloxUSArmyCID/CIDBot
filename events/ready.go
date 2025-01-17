5f9442826a33a5f06982b6b81bfd3e625eb480f0
Filip Hoffmann <folosp2@gmail.com>
2025-01-17 23:30:05 +0100

    add debug logs, gotta do it everywhere else

--- events/ready.go
+++ events/ready.go
@@ -10,28 +10,35 @@ import (
 )
 
 func Ready(discord *discordgo.Session, event *discordgo.Ready) {
-	err := discord.UpdateCustomStatus("Background checking...")
+	status := "Background checking..."
+
+	slog.Debug("updating status", "status", status)
+	err := discord.UpdateCustomStatus(status)
 	if err != nil {
 		slog.Error("could not set custom status", "err", err)
 		os.Exit(1)
 	}
 
+	slog.Debug("registering global commands")
 	_, err = discord.ApplicationCommandBulkOverwrite(event.Application.ID, "", commands.Commands)
 	if err != nil {
-		slog.Error("could not register commands", "err", err)
+		slog.Error("could not register global commands", "err", err)
 		os.Exit(1)
 	}
 
+	slog.Debug("registering server commands")
 	_, err = discord.ApplicationCommandBulkOverwrite(event.Application.ID, config.Configuration.AdminServerID, commands.ServerCommands)
 	if err != nil {
-		slog.Error("could not register admin commands", "err", err)
+		slog.Error("could not register server commands", "err", err)
 		os.Exit(1)
 	}
 
+	slog.Debug("creating whitelist file")
 	file, err := os.OpenFile(config.Configuration.WhitelistPath, os.O_CREATE|os.O_APPEND|os.O_WRONLY, 0644)
 	if err != nil {
 		slog.Error("couldn't create the whitelist file", "err", err)
 		os.Exit(1)
 	}
+	
 	defer file.Close()
 }
