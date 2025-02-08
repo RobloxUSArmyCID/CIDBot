83f340b3c7410cbbb24bd1f7a641f75dffbf6613
Filip Hoffmann <folosp2@gmail.com>
2025-02-08 01:26:00 +0100

    remove useless home manager input

--- flake.nix
+++ flake.nix
@@ -4,17 +4,12 @@
   inputs = {
     nixpkgs.url = "nixpkgs/nixos-unstable";
     flake-utils.url = "github:numtide/flake-utils";
-    home-manager = {
-      url = "github:nix-community/home-manager";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
   };
 
   outputs = {
     self,
     nixpkgs,
     flake-utils,
-    home-manager,
     ...
   }:
     flake-utils.lib.eachDefaultSystem (system: let
